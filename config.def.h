1294633e2932d5d2195f4dc4bb2aa6cae6d5d301
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-07-09 01:39:31 -0300

    Better settings for using with Ubuntu Mono

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=24:style=Medium:antialias=true:autohint=true";
+static char *font = "monospace:size=23:style=Medium:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
